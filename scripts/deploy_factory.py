bb66d05eea9130b98220fd6a099a1c7280586681
Edward Amor <edward.amor3@gmail.com>
2023-08-28 16:12:34 -0400

    fix: deploy script

--- scripts/deploy_factory.py
+++ scripts/deploy_factory.py
@@ -4,25 +4,24 @@ from brownie import (
     Factory,
     LiquidityGauge,
     accounts,
+    ZERO_ADDRESS,
     network,
 )
 
-WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-FEE_RECEIVER = "0xeCb456EA5365865EbAb8a2661B0c503410e9B347"
+WETH = ZERO_ADDRESS
+FEE_RECEIVER = "0xe8269B33E47761f552E1a3070119560d5fa8bBD6"
 
 
 def main():
-    accounts.load("babe")
-    txparams = {"from": accounts[0]}
-    if network.show_active() == "mainnet":
-        txparams.update({"required_confs": 5, "priority_fee": "2 gwei"})
+    dev = accounts.load("dev")
+    txparams = {"from": dev, "priority_fee": "auto"}
 
     token = CurveTokenV5.deploy(txparams)
     pool = CurveCryptoSwap2ETH.deploy(WETH, txparams)
-    gauge = LiquidityGauge.deploy(txparams)
+    gauge = ZERO_ADDRESS
     factory = Factory.deploy(FEE_RECEIVER, pool, token, gauge, WETH, txparams)
 
     print("Token template:", token.address)
     print("Pool template:", pool.address)
-    print("Gauge template:", gauge.address)
+    # print("Gauge template:", gauge.address)
     print("Factory template:", factory.address)
